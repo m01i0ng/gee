9b63258258a3831fb62d130368a5f507f24331f2
m01i0ng <alley.ma@qq.com>
2021-01-11 15:44:01 +0800

    feat: 添加路由分组

--- gee.go
+++ gee.go
@@ -1,29 +1,23 @@
 package gee
 
 import (
+  "log"
   "net/http"
 )
 
 type HandlerFunc func(ctx *Context)
 
 type Engine struct {
+  *RouterGroup
   router *router
+  groups []*RouterGroup
 }
 
 func New() *Engine {
-  return &Engine{router: newRouter()}
-}
-
-func (e *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-  e.router.addRoute(method, pattern, handler)
-}
-
-func (e *Engine) GET(pattern string, handler HandlerFunc) {
-  e.addRoute("GET", pattern, handler)
-}
-
-func (e Engine) POST(pattern string, handler HandlerFunc) {
-  e.addRoute("POST", pattern, handler)
+  e := &Engine{router: newRouter()}
+  e.RouterGroup = &RouterGroup{engine: e}
+  e.groups = []*RouterGroup{e.RouterGroup}
+  return e
 }
 
 func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
@@ -33,4 +27,36 @@ func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
 func (e *Engine) Run(addr string) error {
   return http.ListenAndServe(addr, e)
+}
+
+type RouterGroup struct {
+  prefix      string
+  middlewares []HandlerFunc
+  parent      *RouterGroup
+  engine      *Engine
+}
+
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+  e := group.engine
+  newGroup := &RouterGroup{
+    prefix: group.prefix + prefix,
+    parent: group,
+    engine: e,
+  }
+  e.groups = append(e.groups, newGroup)
+  return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+  pattern := group.prefix + comp
+  log.Printf("Route %4s -%s", method, pattern)
+  group.engine.router.addRoute(method, pattern, handler)
+}
+
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+  group.addRoute("GET", pattern, handler)
+}
+
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+  group.addRoute("POST", pattern, handler)
 }
